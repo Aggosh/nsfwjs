e0c8d9be437a0d71000f722db7ec4128fbf96078
Aggosh <bewerlyproduction@gmail.com>
2023-06-27 13:25:42 +0300

    update node-server part

--- example/django_demo/user_interface/managers/image_score_manager.py
+++ example/django_demo/user_interface/managers/image_score_manager.py
@@ -9,15 +9,19 @@ from user_interface.models import Property, ImageProperty, Image as ImageModel
 
 
 class ImageScoreManager:
-    def score_image(self, image: Image) -> list:
-        url = f"{os.environ['NODE_SERVER_URL']}:8080/nsfw"
-        file_image = {"image": image}
+    def score_image(self, image: Image) -> dict:
+        url = f"{os.environ['NODE_SERVER_URL']}:8080/single/multipart-form"
+        file_image = {"content": image}
 
         res = requests.post(url, files=file_image)
 
         return json.loads(res.text)
 
     def _save_scores(self, response: list, image_model: ImageModel) -> list:
+        prediction = response.get("prediction")
+        if not prediction:
+            raise ValueError("Unknown server response")
+
         image_properties = []
 
         for property in response:
